9fc5aef48ea78a5f1c4c24f99e425f99ead86900
Scrrf <58058628+scrrf2464@users.noreply.github.com>
2025-03-12 15:21:18 +0800

    修改Multi-head-self-attention，加入参数显示

--- self-attention/mult-head-attention.py
+++ self-attention/mult-head-attention.py
@@ -1,14 +1,14 @@
 import math
 import torch
 import torch.nn as nn
-
+from torchsummary import summary
 class MultiHeadSelfattention(nn.Module):
-    def __init__(self, hidden_dim, head_num, attention_dropout=0.1, attention_mask=None):
+    def __init__(self, hidden_dim, head_num, attention_dropout=0.1):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.head_num = head_num
         self.head_dim = hidden_dim // head_num #head_dim做selfattention
-        self.attention_mask = attention_mask
+        # self.attention_mask = attention_mask
         self.q_proj = nn.Linear(hidden_dim, hidden_dim)
         self.k_proj = nn.Linear(hidden_dim, hidden_dim)
         self.v_proj = nn.Linear(hidden_dim, hidden_dim)
@@ -16,7 +16,7 @@ class MultiHeadSelfattention(nn.Module):
 
         self.dropout = nn.Dropout(attention_dropout)
 
-    def forward(self, X):
+    def forward(self, X, attention_mask=None):
         batch, seq_len, h = X.size()
         Q = self.q_proj(X)
         K = self.k_proj(X)
@@ -29,9 +29,9 @@ class MultiHeadSelfattention(nn.Module):
 
         attention_weight = torch.matmul(q_state,k_state.transpose(-1,-2))/math.sqrt(self.head_dim)
 
-        if self.attention_mask is not None:
+        if attention_mask is not None:
             attention_weight = attention_weight.masked_fill(
-                self.attention_mask == 0, float('-inf')
+                attention_mask == 0, float('-inf')
             )
 
         attention_weight = torch.softmax(attention_weight, -1)
@@ -39,11 +39,29 @@ class MultiHeadSelfattention(nn.Module):
 
         output_mid = torch.matmul(attention_weight, v_state)
 
-        output_mid = output_mid.transpose(1,2).comtiguous()
+        output_mid = output_mid.transpose(1,2).contiguous()
         output_mid = output_mid.view(batch, seq_len, -1)
 
         output = self.output_proj(output_mid)
 
         return output
 
+if __name__ == '__main__':
+    attention_mask = (
+        torch.tensor(
+            [
+                [0, 1],
+                [0, 0],
+                [1, 0],
+            ]
+        )
+        .unsqueeze(1)
+        .unsqueeze(2)
+        .expand(3, 8, 2, 2)
+    )
 
+    x = torch.rand(3, 2, 128)
+    net = MultiHeadSelfattention(128, 8)
+    # print(net(x, attention_mask).shape)
+    # print(net)
+    summary(net, input_size=(2, 128), batch_size=5)
